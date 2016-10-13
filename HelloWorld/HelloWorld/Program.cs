9198b92d9b9d59c7c3cd3d3b5b9dc0c6e2e16f81
arapova <arapova.11@mail.ru>
2016-10-13 20:21:07 +0400

    hello World

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+         
+        string userName;
+ 		if (args.Length > 0)
+ 		{
+ 			userName = args[0];
+			}
+		else
+ 			{
+ 				Console.Write("What's your name? ");
+ 				userName = Console.ReadLine();
+ 		};
+ 			Console.WriteLine("Hello, " + userName);
+            Console.Write("\nPrint any key to exit");
+            Console.ReadKey();
+ }
+        }
+    }
+
